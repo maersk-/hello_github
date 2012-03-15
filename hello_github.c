3b90961ec7fb0b3db9e43803c1536ab970fae6cf
Marcel Schwaiger <maersk@gmx.ch>
2012-03-15 17:40:39 +0100

    first commit in github test project"

---
+++ hello_github.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : hello_github.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("hello github");
+	return EXIT_SUCCESS;
+}
